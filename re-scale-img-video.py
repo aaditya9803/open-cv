e97ff55bd31943ecc894a0d0789d64a3331aeb3b
Aaditya Neupane <aaditya9803@gmail.com>
2024-02-21 18:58:06 +0100

    for live/captured video&pictures

--- re-scale-img-video.py
+++ re-scale-img-video.py
@@ -2,11 +2,16 @@ import cv2 as cv
 cat = cv.imread('Photos/cat.jpg')
 cv.imshow('cat',cat)
 
-def rescaleFrame(frame, scale = 0.75):
+def rescaleFrame(frame, scale = 0.75): #For the Captured video and Photos
     width = int(frame.shape[1]*scale)
     height = int(frame.shape[0]*scale)
     dimension =  (width,height)
     return cv.resize(frame, dimension, interpolation=cv.INTER_AREA)
+
+def changeResolution(width,height): #For the Live video
+    capture.set(3,width) #3 reference width and 4 reference the height
+    capture.set(4,height)
+
 cv.imshow('Image',rescaleFrame(cat))
 capture = cv.VideoCapture("Videos/dog.mp4")
 while True:
