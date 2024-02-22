bfb3705a4e173966f59d3edd7f75e618b9e43bf0
Aaditya Neupane <aaditya9803@gmail.com>
2024-02-22 14:59:40 +0100

    Contours

--- identify-contours.py
+++ identify-contours.py
@@ -1,21 +1,29 @@
 import cv2 as cv
+import numpy as np
+
 img = cv.imread('Photos/cat.jpg')
 cv.imshow('A Cat', img)
-
-#Converrting to gray and blur
+blank = np.zeros(img.shape, dtype='uint8')
+cv.imshow('Blank', blank)
+# #Converrting to gray and blur
 gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-gray = cv.GaussianBlur(gray, (5,5), cv.BORDER_DEFAULT)
+# gray = cv.GaussianBlur(gray, (5,5), cv.BORDER_DEFAULT)
 cv.imshow('A Cat Gray and Blur', gray)
-#Canny
+# #Canny
 canny = cv.Canny(gray,125,175)
 cv.imshow('A Canny cat', canny)
 
 
 #Contours
-
-contours, hierarchies = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE) 
+ret, thresh = cv.threshold(gray, 125, 255, cv.THRESH_BINARY)
+cv.imshow('Thresholded Image', thresh)
+contours, hierarchies = cv.findContours(thresh, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE) 
 #RETR_LIST - retrieves all of the contours in the canny image
 #CHAIN_APPROX_NONE - retrieves all of the contours points
 print(f'{len(contours)} No. contours found')
+
+
+cv.drawContours(blank, contours, -1, (0,0,255), 1)
+cv.imshow('Contours Cat', blank)
 
 cv.waitKey(0)
